252342b9717173df3b2272bd82afc3792f921c50
danielbeinat <danielbeinat@outlook.es>
2022-10-12 14:42:32 -0300

    Ventana con SFML

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Blue);
+
+
+        window.display();
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
